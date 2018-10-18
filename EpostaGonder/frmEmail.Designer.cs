78b3a99d672b51546df7ff1fddb24539a040893a
Emre Yaşa <emreyasa07@windowslive.com>
2018-10-18 21:06:13 +0300

    Eposta gönderimi kaynak kodları.

---
+++ EpostaGonder/frmEmail.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace EpostaGonder
+{
+    partial class frmEmail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbGmail = new System.Windows.Forms.RadioButton();
+            this.rbHotmail = new System.Windows.Forms.RadioButton();
+            this.rbYahoo = new System.Windows.Forms.RadioButton();
+            this.gb01 = new System.Windows.Forms.GroupBox();
+            this.gb01.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEmail.Location = new System.Drawing.Point(12, 38);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(181, 17);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "Gönderim Yapacak Eposta:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPassword.Location = new System.Drawing.Point(12, 67);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(224, 17);
+            this.lblPassword.TabIndex = 7;
+            this.lblPassword.Text = "Gönderim Yapacak Eposta Şifresi:";
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGonder.Location = new System.Drawing.Point(15, 127);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(76, 32);
+            this.btnGonder.TabIndex = 9;
+            this.btnGonder.Text = "Gönder";
+            this.btnGonder.UseVisualStyleBackColor = true;
+            this.btnGonder.Click += new System.EventHandler(this.btnGonder_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtEmail.Location = new System.Drawing.Point(242, 35);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(223, 23);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPassword.Location = new System.Drawing.Point(242, 64);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(223, 23);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ad Soyad:";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdSoyad.Location = new System.Drawing.Point(242, 6);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(223, 23);
+            this.txtAdSoyad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Eposta Servisi:";
+            // 
+            // rbGmail
+            // 
+            this.rbGmail.AutoSize = true;
+            this.rbGmail.Checked = true;
+            this.rbGmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbGmail.Location = new System.Drawing.Point(6, 19);
+            this.rbGmail.Name = "rbGmail";
+            this.rbGmail.Size = new System.Drawing.Size(62, 21);
+            this.rbGmail.TabIndex = 10;
+            this.rbGmail.TabStop = true;
+            this.rbGmail.Text = "Gmail";
+            this.rbGmail.UseVisualStyleBackColor = true;
+            // 
+            // rbHotmail
+            // 
+            this.rbHotmail.AutoSize = true;
+            this.rbHotmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbHotmail.Location = new System.Drawing.Point(71, 19);
+            this.rbHotmail.Name = "rbHotmail";
+            this.rbHotmail.Size = new System.Drawing.Size(73, 21);
+            this.rbHotmail.TabIndex = 11;
+            this.rbHotmail.Text = "Hotmail";
+            this.rbHotmail.UseVisualStyleBackColor = true;
+            // 
+            // rbYahoo
+            // 
+            this.rbYahoo.AutoSize = true;
+            this.rbYahoo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbYahoo.Location = new System.Drawing.Point(150, 19);
+            this.rbYahoo.Name = "rbYahoo";
+            this.rbYahoo.Size = new System.Drawing.Size(67, 21);
+            this.rbYahoo.TabIndex = 12;
+            this.rbYahoo.Text = "Yahoo";
+            this.rbYahoo.UseVisualStyleBackColor = true;
+            // 
+            // gb01
+            // 
+            this.gb01.Controls.Add(this.rbGmail);
+            this.gb01.Controls.Add(this.rbYahoo);
+            this.gb01.Controls.Add(this.rbHotmail);
+            this.gb01.Location = new System.Drawing.Point(242, 97);
+            this.gb01.Name = "gb01";
+            this.gb01.Size = new System.Drawing.Size(223, 52);
+            this.gb01.TabIndex = 13;
+            this.gb01.TabStop = false;
+            // 
+            // frmEmail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 172);
+            this.Controls.Add(this.gb01);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnGonder);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEmail";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UEFA Eposta Gönderimi";
+            this.Load += new System.EventHandler(this.frmEmail_Load);
+            this.gb01.ResumeLayout(false);
+            this.gb01.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbGmail;
+        private System.Windows.Forms.RadioButton rbHotmail;
+        private System.Windows.Forms.RadioButton rbYahoo;
+        private System.Windows.Forms.GroupBox gb01;
+    }
+}
+
---
+++ EpostaGonder/frmEmail.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace EpostaGonder
+{
+    public partial class frmEmail : Form
+    {
+        public frmEmail()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEmail_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGonder_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                MessageBox.Show("Gönderim yapacak e-posta adresini giriniz.", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtEmail.Select();
+                return;
+            }
+
+            Regex rgx = new Regex("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$");
+            if (!rgx.IsMatch(txtEmail.Text))
+            {
+                MessageBox.Show("Gönderim yapacak e-posta adresi doğru formatta değil.", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtEmail.Select();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtPassword.Text))
+            {
+                MessageBox.Show("Gönderim yapacak e-posta adresinin şifresini giriniz.", "HATA!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPassword.Select();
+                return;
+            }
+
+            string emailBodyFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "email-body.txt");
+            FileInfo emailBodyFileInfo = new FileInfo(emailBodyFilePath);
+            if (!emailBodyFileInfo.Exists)
+            {
+                MessageBox.Show("Gönderim yapılacak e-posta adresleri bulunamadı.", "HATA!",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string emailListFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "email-list.txt");
+            FileInfo emailListFileInfo = new FileInfo(emailListFilePath);
+            if (!emailListFileInfo.Exists)
+            {
+                MessageBox.Show("Gönderim yapılacak e-posta adresleri bulunamadı.", "HATA!",  MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            string[] emailBody = File.ReadAllLines(emailBodyFileInfo.FullName);
+            foreach (var text in emailBody)
+                sb.Append(text);
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials = new NetworkCredential(txtEmail.Text, txtPassword.Text);
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+
+            if (rbGmail.Checked)
+                smtp.Host = "smtp.gmail.com";
+
+            if (rbHotmail.Checked)
+                smtp.Host = "smtp.live.com";
+
+            if (rbYahoo.Checked)
+                smtp.Host = "smtp.mail.yahoo.com";
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.Body = sb.ToString();
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Priority = MailPriority.High;
+            mailMessage.BodyEncoding = Encoding.UTF8;
+            mailMessage.SubjectEncoding = Encoding.UTF8;
+
+            string displayName = string.IsNullOrEmpty(txtAdSoyad.Text) ? txtEmail.Text : txtEmail.Text;
+            mailMessage.From = new MailAddress(txtEmail.Text, displayName);
+            mailMessage.Subject = "About Galatasaray SK Case";
+
+            string[] emailList = File.ReadAllLines(emailListFileInfo.FullName);
+            foreach (string email in emailList)
+            {
+                if (!rgx.IsMatch(email))
+                    continue;
+
+                mailMessage.To.Add(new MailAddress(email));
+
+                try
+                {
+                    smtp.Send(mailMessage);
+                }
+                catch (SmtpException ex)
+                {
+                    MessageBox.Show(
+                        string.Format("{0} adresine e-posta gönderiminde hata oluştu: {1}", email, ex.Message), 
+                        "HATA!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        string.Format("{0} adresine e-posta gönderiminde hata oluştu: {1}", email, ex.Message),
+                        "HATA!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            MessageBox.Show("E-posta gönderimi tamamlandı!", "Başarı!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
